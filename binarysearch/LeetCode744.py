84dfcd23a9d78c58991abc55d206543af40a70b1
zl_yunqing163 <yunqing_71@163.com>
2020-08-20 10:23:07 +0800

    LeetCode744 寻找比目标字母大的最小字母（附带官方题解）

--- binarysearch/LeetCode744.py
+++ binarysearch/LeetCode744.py
@@ -12,6 +12,7 @@
 # here put the import lib
 from typing import List
 class Solution:
+    # 自己写的方法，比较笨
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         # 小写字母 = 97 - 122
         # 转换ASCII ord()
@@ -37,7 +38,21 @@ class Solution:
                 pass
         return letters[index]
 
+    # 照着官方题解的方法
+    def nextGreatestLetter_1(self, letters: List[str], target: str) -> str:
+        low, high = 0, len(letters)
+        # 求出要插入target到列表中应该插入到的位置
+        while low < high:
+            mid = (low + high) // 2
+            if letters[mid] <= target:
+                low = mid + 1
+            else:
+                high = mid
+        # 巧妙的运用取模找到对应结果
+        return letters[low % len(letters)]
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.nextGreatestLetter(["c","f","j"], 'd'))
+    arr, target = ["c","f","j"], 'v'
+    print(s.nextGreatestLetter(arr, target))
+    print(s.nextGreatestLetter_1(arr, target))
